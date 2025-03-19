70ec696e8fca9bf4644f38e357bb0a93a564f12c
Nobody-IArc <utk3970@naver.com>
2025-03-19 13:23:04 +0900

    Add Model, Test Code & Update Service, Controller

--- src/blog.controller.ts
+++ src/blog.controller.ts
@@ -6,34 +6,52 @@ import {
   Get,
   Post,
   Put,
-} from '@nestjs/common';
+} from '@nestjs/common'; // Nest.js 컨트롤러 요소 Import
 
-@Controller('blog')
+import { BlogService } from './blog.service';
+import { PostDto } from './blog.model';
+
+@Controller('blog') // Controller Decorator
+
+// export BlogController 로 다른 코드에서 사용 가능
 export class BlogController {
-  @Get()
+  blogService: BlogService;
+
+  // 생성자로 blogService 생성
+  // 추후에 의존성 주입 방식으로 변경
+  constructor() {
+    this.blogService = new BlogService();
+  }
+
+  @Get() // Get Decorator, 서버 경로에 대해 getAllPosts()
   getAllPosts() {
     console.log('Get all posts');
+    return this.blogService.getAllPosts(); // Service 에서 가져온 메서드
   }
 
-  @Post()
-  createPost(@Body() post: any) {
+  @Post() // 서버 경로에 createPost()
+  createPost(@Body() postDto: PostDto) {
     console.log('Create new Post');
-    console.log(post);
+    this.blogService.createPost(postDto); // Service 에서 가져온 메서드
+    return 'success';
   }
 
-  @Get('/:id')
+  @Get('/:id') // '/:id' 경로에 getPost()
   getPost(@Param('id') id: string) {
     console.log(`Get [id: ${id}]'s Post`);
+    return this.blogService.getPost(id); // Service 에서 가져온 메서드
   }
 
-  @Delete('/:id')
-  deletePost() {
+  @Delete('/:id') // '/:id' 경로에 deletePost()
+  deletePost(@Param('id') id: string) {
     console.log('Delete post');
+    this.blogService.deletePost(id); // Service 에서 가져온 메서드
+    return 'success';
   }
 
-  @Put('/:id')
-  updatePost(@Param('id') id, @Body() post: any) {
-    console.log(`Update [id: ${id}]'s Post`);
-    console.log(post);
+  @Put('/:id') // '/:id' 경로에 updatePost
+  updatePost(@Param('id') id: string, @Body() postDto: PostDto) {
+    console.log('Update Post ', id, postDto);
+    return this.blogService.updatePost(id, postDto); // Service 에서 가져온 메서드
   }
 }
--- src/blog.service.ts
+++ src/blog.service.ts
@@ -1,1 +1,37 @@
-export class BlogService {}
+import { PostDto } from './blog.model'; // Model Import
+
+export class BlogService {
+  posts: PostDto[] = [];
+
+  getAllPosts() {
+    return this.posts;
+  }
+
+  // Omit 으로 'id', 'createdAt' 속성 제외 후 가져오기
+  createPost(postDto: Omit<PostDto, 'id' | 'createdAt'>) {
+    const newId: string = (this.posts.length + 1).toString();
+    const newPost: PostDto = { id: newId, createdAt: new Date(), ...postDto };
+    this.posts.push(newPost);
+  }
+
+  getPost(id: any) {
+    const post = this.posts.find((post) => {
+      return post.id === id;
+    });
+    console.log(post);
+    return post;
+  }
+
+  deletePost(id: any) {
+    const filteredPosts = this.posts.filter((post) => post.id !== id);
+    this.posts = [...filteredPosts];
+  }
+
+  // Omit 으로 'id' 가져오지 않기
+  updatePost(id: string, postDto: Omit<PostDto, 'id'>) {
+    const updateIndex = this.posts.findIndex((post) => post.id === id);
+    const updatePost = { id, ...postDto, updatedAt: new Date() };
+    this.posts[updateIndex] = updatePost;
+    return updatePost;
+  }
+}
